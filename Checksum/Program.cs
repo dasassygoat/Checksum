249ec024d9308454bd9dd9e7f98e41318fa1246a
journeyman32 <bryan@coderbox.co>
2018-11-20 01:34:38 -0500

    Added exception catch and basic file path logic

--- Checksum/Program.cs
+++ Checksum/Program.cs
@@ -9,39 +9,69 @@ namespace Checksum
     {
         static void Main(string[] args)
         {
-            var file = new FileInfo(args[0]);
-            using (var encodedFile = new SHA256Managed())
+            while (args.Length < 1)
             {
-                using (FileStream fStream = file.Open(FileMode.Open))
+                Console.Write("File path ==> ");
+                var inputPath = Console.ReadLine();
+                if (inputPath.Trim() != "")
                 {
-                    fStream.Position = 0;
-                    var hash = encodedFile.ComputeHash(fStream);
-
-                    System.Console.WriteLine(getGashString(hash));
+                    args = new string[] { inputPath.Trim() };
                 }
             }
-
-                
-            //Console.ReadKey();
+            var file = new FileInfo(args[0]);
+            var format = "X2";
+            var sha256 = new SHA256Managed();
+            EncodeFile<SHA256>(sha256, file, format);
         }
 
-        private static string getGashString(byte[] hash)
+        /// <summary>
+        /// Generic function to actually do the encoding of the file with the specified hash algorithm
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="encryptionType"></param>
+        /// <param name="file"></param>
+        /// <param name="fmt"></param>
+        private static void EncodeFile<T>(T encryptionType, FileInfo file, string fmt = "") where T : HashAlgorithm
+        {
+            try
+            {
+                using (T encodedFile = encryptionType)
+                {
+                    using (FileStream fStream = file.Open(FileMode.Open))
+                    {
+                        fStream.Position = 0;
+                        var hash = encodedFile.ComputeHash(fStream);
+
+                        System.Console.WriteLine(getGashString(hash, fmt));
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("File was unable to be used for hashing.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured during operaion. Please see following exception for more information.");
+                Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// Convert each element of the hash byte array into a string using the option fmt string parameter 
+        /// </summary>
+        /// <param name="hash"></param>
+        /// <param name="fmt"></param>
+        /// <returns></returns>
+        private static string getGashString(byte[] hash, string fmt)
         {
             StringBuilder outVal = new StringBuilder();
-            foreach(var b in hash)
+            foreach (var b in hash)
             {
-                outVal.Append(b.ToString("X2"));
+                outVal.Append(b.ToString(fmt));
             }
 
             return outVal.ToString();
         }
     }
 }
-
-
-//for (int i = 0; i<array.Length; i++)
-//        {
-//            Console.Write($"{array[i]:X2}");
-//            if ((i % 4) == 3) Console.Write(" ");
-//        }
-//        Console.WriteLine();
